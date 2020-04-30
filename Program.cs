8a98b6f183521580ee2e9703f9cdcc92820370fe
Christian <voschristian@gmail.com>
2020-04-30 22:07:09 +0200

    hello masters in master

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitVoorbeeld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! master 10");
+            Console.WriteLine("Hello World! hello masters");
             Console.ReadKey(true);
         }
     }
